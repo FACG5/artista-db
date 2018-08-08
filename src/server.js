eb3ce7126cc3736bd9a5fca7577c8d7600f256ff
Ali <36124895+ali-7@users.noreply.github.com>
2018-08-08 16:56:57 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const router = require('./router');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 http.createServer(router).listen(port, () => {
   console.log(`we now in port : ${port}`);
